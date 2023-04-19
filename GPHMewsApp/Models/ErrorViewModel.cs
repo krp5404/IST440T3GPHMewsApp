7b25cd2ba5d4eb3c47f8bc98c35898382a2cc7ec
krp5404 <krp5404@L7ABWLD345-246>
2023-04-19 14:34:28 -0400

    Add project files.

---
+++ GPHMewsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GPHMewsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
