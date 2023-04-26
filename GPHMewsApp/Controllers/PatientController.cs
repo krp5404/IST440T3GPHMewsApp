07ea387273fbab10177c24fd99d9c40bfe2104ac
krp5404 <kjlpatel58@gmail.com>
2023-04-25 21:49:05 -0400

    Added Controllers and Views for Patients and Clinicians

---
+++ GPHMewsApp/Controllers/PatientController.cs
@@ -0,0 +1,30 @@
+﻿using GPHMewsApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GPHMewsApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly DataDbContext gphDataDbContext;
+
+        public PatientController(DataDbContext gphDataDbContext)
+        {
+            this.gphDataDbContext = gphDataDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PatientIndex()
+        {
+            var patients = await gphDataDbContext.Patients.ToListAsync();
+            return View(patients);
+        }
+
+
+    }
+}
